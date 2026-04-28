b07ef119efdb51a1e38b6c991f5fd662074aa1bd
mahakkushwah2409-creator <mahakkushwah2409@gmail.com>
2026-04-28 09:25:20 +0530

    Comrehensive-Viva

---
+++ LogisticRouteTracker/FuelCheckpoint.java
@@ -0,0 +1,21 @@
+package com.LogisticRouteTracker;
+
+
+    class FuelCheckpoint extends Checkpoint {
+        public FuelCheckpoint(String id, String loc, double dist, int exp, int act) {
+            super(id, loc, dist, exp, act);
+        }
+
+        boolean isCritical() {
+            return true;
+        }
+
+        String getType() {
+            return "FuelCheckpoint";
+        }
+
+        double calculatePenalty() {
+            return isDelayed() ? 10 : 0;
+        }
+    }
+
